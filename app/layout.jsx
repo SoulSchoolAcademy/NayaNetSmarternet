409c35ceb5a897cb083bdc43622c3938fc55ebb0
Shawn Vibert <humanmaximuscodex@gmail.com>
2025-08-12 15:14:19 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,10 @@
 export const metadata = { title: "SmartNet", description: "SmartNet" };
-
 import "./globals.css";
-import Sidebar from "../components/Sidebar";
-import Topbar from "../components/Topbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div className="min-h-screen grid grid-cols-[260px_1fr]">
-          <Sidebar />
-          <div className="flex flex-col">
-            <Topbar />
-            <main className="container">{children}</main>
-          </div>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
