7dda442cc6fa3773ec1e00bf7131a96d275f2a2d
Shawn Vibert <humanmaximuscodex@gmail.com>
2025-08-12 14:35:01 -0700

    Update AppIcon.jsx

--- src/icons/AppIcon.jsx
+++ src/icons/AppIcon.jsx
@@ -3,6 +3,20 @@ import { ICONS } from "./registry";
 
 export default function AppIcon({ name, size = 56, alt, className }) {
   const src = ICONS[name] || "/logo.svg";
+  const isSVG = src.toLowerCase().endsWith(".svg");
+
+  if (isSVG) {
+    return (
+      <img
+        src={src}
+        alt={alt ?? name}
+        width={size}
+        height={size}
+        className={className}
+        style={{ display: "inline-block" }}
+      />
+    );
+  }
   return (
     <Image
       src={src}
