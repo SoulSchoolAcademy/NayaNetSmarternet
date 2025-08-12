8dea2dadaa90244e66051fb69d4b1b63153eb652
Shawn Vibert <humanmaximuscodex@gmail.com>
2025-08-12 15:18:54 -0700

    Update layout.jsx

--- app/(hub)/layout.jsx
+++ app/(hub)/layout.jsx
@@ -1,1 +1,14 @@
+import Topbar from "../../components/Topbar";
+import HubSidebar from "../../components/HubSidebar";
 
+export default function HubLayout({ children }) {
+  return (
+    <div className="min-h-screen grid grid-cols-[260px_1fr]">
+      <HubSidebar />
+      <div className="flex flex-col">
+        <Topbar />
+        <main className="container">{children}</main>
+      </div>
+    </div>
+  );
+}
